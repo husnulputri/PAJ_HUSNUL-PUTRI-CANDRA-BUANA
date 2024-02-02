7ab8cb289c55f34388c6abe1f2c7a18093b97337
husnulputri <husnulputricandra@gmail.com>
2024-02-02 10:02:48 +0700

    first commit

--- minggu_1/routes/web.php
+++ minggu_1/routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/hai', function () {
+    return 'Hello World';
 });
+
+Route::get('/about', function () {
+    return view('about');
+ });
+
+Route::get('/servis', function () {
+    return view('servis');
+});
+
+
