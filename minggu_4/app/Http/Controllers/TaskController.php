c8438ebabac12f0033b859f44bfb577c489e851a
husnulputri <82297217+husnulputri@users.noreply.github.com>
2024-02-23 09:47:53 +0700

    minggu 4

--- minggu_4/app/Http/Controllers/TaskController.php
+++ minggu_4/app/Http/Controllers/TaskController.php
@@ -9,7 +9,8 @@ use App\Models\Task;
 class TaskController extends Controller
 {
     public function index(){
-        return view('task.index');
+        $task = Task::all();
+        return view('task.index',compact('task'));
     }
     public function create(){
         return view('task.create');
@@ -20,5 +21,24 @@ class TaskController extends Controller
         Task::create($validatedData);
         return redirect('/task');
     }
+
+    public function show(Task $task){
+        return view('task.show',compact('task'));
+    }
+
+    public function edit($id){
+        $task= Task::findOrFail($id);
+        return view('task.edit', compact('task'));
+    }
+
+    public function update(Request $request, $id){
+        $task = Task::findOrFail($id);
+        $task->update($request->all());
+        return redirect('/task');
+    }
+
+
+
+    
     
 }
